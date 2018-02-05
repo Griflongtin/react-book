d3ce87e428575785b9d57b5710b3d1b5a499a753
Griffin P Longtin and Mikey Pinaud <griffin@>
2018-02-05 11:52:41 -0800

    add NewsFeed component

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import NewsFeed from "./news-feed-component/NewsFeed"
+
+function App() {
+  return (
+    <div>
+      <NewsFeed />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/news-feed-component/NewsFeed.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewsFeed() {
+  return(
+    <div>
+      <h1>NewsFeed Works!</h1>
+    </div>
+  );
+}
+
+export default NewsFeed;
